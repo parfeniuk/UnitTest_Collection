e47941669090aea14ee2e5fa91691cd57dee6c61
Oleksandr P <o.parfeniuk@gmail.com>
2017-09-29 11:31:22 +0300

    IEqualityComparer implemented on class Product

--- ShopcartLibrary/Product.cs
+++ ShopcartLibrary/Product.cs
@@ -6,12 +6,45 @@ using System.Threading.Tasks;
 
 namespace ShopcartLibrary
 {
-    public class Product
+    public class Product: EqualityComparer<Product>, IEquatable<Product>
     {
         public int ProductID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string Category { set; get; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            Product prod = obj as Product;
+            if (prod == null) return false;
+            else return Equals(prod);
+        }
+
+        public bool Equals(Product other)
+        {
+            if (other == null) return false;
+            return Name == other.Name && Price == other.Price && Category == other.Category;
+        }
+
+        public override bool Equals(Product x, Product y)
+        {
+            if (x == null && y == null) return true;
+            else if (x == null || y == null) return false;
+
+            if (x.Equals(y)) return true;
+            else return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return GetHashCode(this);
+        }
+
+        public override int GetHashCode(Product obj)
+        {
+            return obj.Price.GetHashCode() ^ obj.Category.GetHashCode() ^ obj.Name.GetHashCode();
+        }
     }
 }
--- ShopcartLibraryTests/ShoppingCartInitializeTests.cs
+++ ShopcartLibraryTests/ShoppingCartInitializeTests.cs
@@ -12,9 +12,12 @@ namespace ShopcartLibrary.Tests
     public class ShoppingCartInitializeTests
     {
         private static ShoppingCart cart;
+        private static Product product;
         [ClassInitialize]
-        public static void ShoppingCartInitializer(TestContext context)
+        public static void ClassInitialize(TestContext context)
         {
+            product = new Product { Name = "Мяч", Category = "Футбол", Price = 19.50M };
+
             Product[] products =
              {
                 new Product { Name = "Каяк", Category = "Водные виды спорта", Price = 275M },
@@ -22,6 +25,7 @@ namespace ShopcartLibrary.Tests
                 new Product { Name = "Мяч", Category = "Футбол", Price = 19.50M },
                 new Product { Name = "Угловой флажок", Category = "Футбол", Price = 34.95M }
             };
+
             cart = new ShoppingCart(new LinqValueCalculator());
             cart.Products = products;
         }
@@ -37,6 +41,46 @@ namespace ShopcartLibrary.Tests
             CollectionAssert.AllItemsAreUnique(cart.Products.ToArray());
         }
         [TestMethod]
-        public void ShoppingCart_AllProducts_
+        public void ShoppingCart_AllProducts_NotNull()
+        {
+            CollectionAssert.AllItemsAreNotNull(cart.Products.ToArray());
+        }
+        [TestMethod]
+        public void ShoppingCart_ProductsContains_Product()
+        {
+            // Using Contains extension method requires 
+            // implementation of IEquitable<Product> in class Product
+            // method bool Equals(Product other) should be defined
+            CollectionAssert.Contains(cart.Products.ToList(), product);
+        }
+        [TestMethod]
+        public void ShoppingCart_AddProduct_Added()
+        {
+            Product item = new Product()
+            {
+                Name = "Bicycle",
+                Category = "Cycling",
+                Price = 2566.3M
+            };
+            int expected = cart.Products.Count() + 1;
+            // Add item to (IEnumerable<Product>) Products
+            // In fact, new IEnumerable<Product> object will be created
+            cart.Products=cart.Products.Concat(new[] {item });
+
+            Assert.AreEqual(expected, cart.Products.Count());
+        }
+        [TestMethod]
+        public void ShoppingCart_RemoveProduct_Removed()
+        {
+            Product item2 = new Product { Name = "Каяк", Category = "Водные виды спорта", Price = 275M };
+            int expected = cart.Products.Count() - 1;
+            // Remove item from (IEnumerable<Product>) Products
+            // Returns a new collection that doesn't contain the excluded items
+            
+            // Requires implementation of IEqualityComparer<Product>
+
+            cart.Products = cart.Products.Except(new[] { item2 });
+            Assert.AreEqual(expected, cart.Products.Count());
+        }
     }
 }
